361a655353c08c73fe99d54dde08edb8fc448c24
havietkok-sys <haviet.kok@gmail.com>
2026-02-22 18:54:00 +0100

    Ändrade bara sökvägen för bilderna i startpage

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -6,11 +6,11 @@ StartPage.route = {
 
 export default function StartPage() {
   return <div className="homepage-images">
-    <img src="/images/movies/PulpFiction.jpg" alt="Pulp Fiction" />
+    <img src="/images/movies/Pulp-Fiction.jpg" alt="Pulp-Fiction" />
     <br /><br />
     <img src="/images/movies/TheLord_of_the_rings.jpg" alt="The Lord of the Rings" />
     <br /><br />
-    <img src="/images/movies/TheGodFather.jpeg" alt="The GodFather" />
+    <img src="/images/movies/The-GodFather.jpg" alt="The GodFather" />
     <br /><br />
     <img src="/images/movies/StarWars.jpg" alt="Star wars" />
     <br /><br />
