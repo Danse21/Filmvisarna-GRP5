ac93c291278773d2358ea99d08187491ba290419
Haviet Kok <haviet.kok@gmail.com>
2026-03-09 01:55:02 +0100

    Var tvungen å återskapa filer för mitt windows vägra släppa gamla identiteter på filer

---
+++ src/utils/booking/mergeSeatsWithBookingState.ts
@@ -0,0 +1,27 @@
+import { generateSeatsFromLayout } from "./generateSeatsFromLayout";
+
+type DbSeat = {
+  id: number;
+  seat_row: number;
+  seat_number: number;
+  is_booked: boolean;
+};
+
+export default function mergeSeatsWithBookingState(
+  layout: number[],
+  dbSeats: DbSeat[],
+) {
+  const uiSeats = generateSeatsFromLayout(layout);
+
+  return uiSeats.map((seat) => {
+    const dbSeat = dbSeats.find(
+      (item) => item.seat_row === seat.row && item.seat_number === seat.seatNumber,
+    );
+
+    return {
+      ...seat,
+      is_booked: dbSeat ? dbSeat.is_booked : false,
+    };
+  });
+}
+
