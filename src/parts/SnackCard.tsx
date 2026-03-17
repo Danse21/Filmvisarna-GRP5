739fb465c711cc74e2673fd0d584e5ea86ab642c
Meti00a <muhamedhajra00@gmail.com>
2026-03-17 12:26:43 +0100

    -Implemented some small things to scss file, since it wasn't visually stunning. -fixed an issue with the structure on the page and card , since the text of the price and the name wasn't structured properly. -also implemented a string to the price and the number.

--- src/parts/SnackCard.tsx
+++ src/parts/SnackCard.tsx
@@ -1,15 +1,24 @@
 
 import '../sass/snackspage.scss';
 
+type Snack = {
+  name: string;
+  price: string;
+};
+
 type Props = {
   snack: typeof SnackCard;
+};
+
+type SnackCard = {
+  name: string;
+  price: number;
 };
 
 export default function SnackCard({ snack }: Props) {
   return (
     <div className="snack-row">
       <span className="snack-name">{snack.name}</span>
-
     </div>
   );
 }
