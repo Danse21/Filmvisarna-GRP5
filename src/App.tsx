4a2198236b3b95e6239293fb728ecc1943734f51
Haviet Kok <haviet.kok@gmail.com>
2026-03-18 13:50:22 +0100

    Satt upp context för user som wrap runt outlet i main.tsx Tog även bort lite gammal slask kod.

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,13 @@ import { useLocation } from "react-router-dom";
 import Header from "./partials/Header";
 import Main from "./partials/Main";
 import Footer from "./partials/Footer";
-// import BootstrapBreakpoints from "./parts/BootstrapBreakpoints";
-import { useState } from "react";
-// turn off when not needed for debugging
-// const showBootstrapBreakpoints = true;
+
 
 export default function App() {
-  // scroll to top when the route changes
   useLocation();
   window.scrollTo({ top: 0, left: 0, behavior: "instant" });
 
-  const [trailerUrl, setTrailerUrl] = useState<string | null>(null);
-  // make footer stick to the bottom even when content is short
-  // and moves down naturally when content is long. Wrap everything in a flex column layout
-  // main content expands (flex-grow-1)
+
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
